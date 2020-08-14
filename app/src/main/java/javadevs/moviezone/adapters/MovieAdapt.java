183c6c337d912804e4f649a7e5fae463a70d9c06
Johnpaul Chukwu <johnpaulchukwu43@mail.com>
2020-08-14 16:19:43 +0100

    code cleanup1 series1:

--- app/src/main/java/javadevs/moviezone/adapters/MovieAdapt.java
+++ app/src/main/java/javadevs/moviezone/adapters/MovieAdapt.java
@@ -6,11 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+
 
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+
+import java.util.List;
 
 import javadevs.moviezone.R;
 import javadevs.moviezone.model.Movie;
@@ -19,50 +20,45 @@ import javadevs.moviezone.model.Movie;
  * Created by CHUKWU JOHNPAUL on 16/04/17.
  */
 
-public class MovieAdapt extends BaseAdapter{
-    private final static String BASE_POSTER_URL="https://image.tmdb.org/t/p/";
-    private static final String IMAGE_SIZE = "w300" ;
-    //
-    LinearLayout mLinearLayout;
+public class MovieAdapt extends BaseAdapter {
+    private static final String BASE_POSTER_URL = "https://image.tmdb.org/t/p/";
+    private static final String IMAGE_SIZE = "w300";
+
     ImageView mImg;
-    //List to hold movies
-    private ArrayList<Movie> myMovie;
-    // generic list that only accepts type of Movie class
+    private List<Movie> movieList;
+
     private Context ctx;
 
-    //Movie Adapt Constructor
-    public MovieAdapt(ArrayList<Movie> myMovie, Context ctx) {
-        this.myMovie = myMovie;
+    public MovieAdapt(List<Movie> movieList, Context ctx) {
+        this.movieList = movieList;
         this.ctx = ctx;
     }
-    //Get total Count
+
     @Override
     public int getCount() {
-        return myMovie.size();
+        return movieList.size();
     }
-    //Get item at specified position
+
     @Override
     public Object getItem(int position) {
-        return myMovie.get(position);
+        return movieList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
         return position;
     }
-    //return the Image view
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Movie mymovie= (Movie) getItem(position);
+        Movie movie = (Movie) getItem(position);
         LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View row;
-        row = inflater.inflate(R.layout.movie_item,parent,false);
-        mImg = (ImageView)row.findViewById(R.id.cover_image);
-        String url= new StringBuilder()
-                .append(BASE_POSTER_URL)
-                .append(IMAGE_SIZE)
-                .append(mymovie.getPosterPath().trim())
-                .toString();
+        row = inflater.inflate(R.layout.movie_item, parent, false);
+        mImg = row.findViewById(R.id.cover_image);
+        String url = BASE_POSTER_URL +
+                IMAGE_SIZE +
+                movie.getPosterPath().trim();
         Picasso.with(ctx)
                 .load(url)
                 .placeholder(R.mipmap.ic_load)
@@ -71,9 +67,9 @@ public class MovieAdapt extends BaseAdapter{
         return row;
     }
 
-    public void cleanUp(){
-        if(myMovie.size() > 0){
-           myMovie.clear();
+    public void cleanUp() {
+        if (!movieList.isEmpty()) {
+            movieList.clear();
         }
     }
 }
